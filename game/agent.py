fc8fb0fe97171f0e0f205dd4c315483bd48c7ef2
Oscar <oscar.wilkinson104@gmail.com>
2023-11-13 15:41:51 +0000

    Importing parts of Generative Agents code (WIP)

--- game/agent.py
+++ game/agent.py
@@ -18,6 +18,8 @@ from game.prompt_helpers import (
 from llm.base import LLMBase
 from schema import ActionCompletion, Conversation, Knowledge, Memory, Message
 
+import spatial_memory
+
 
 class GenAgent:
     def __init__(
@@ -32,6 +34,13 @@ class GenAgent:
         self._conversation_history: List[Message] = []
         self._knowledge = knowledge
         self._conversation_context = Conversation()
+        
+        # TODO: Populate these:
+        self.curr_tile = None
+        self.vision_r = None
+        
+        f_s_mem_saved = "spatial_memory/spatial_memory.json"
+        self.s_mem = spatial_memory.MemoryTree(f_s_mem_saved)
 
     @classmethod
     async def create(
@@ -48,7 +57,7 @@ class GenAgent:
             if self._knowledge.agent_def.uuid in lore.known_by
         ]
 
-        initial_memories += self._knowledge.agent_def.personal_lore
+        initial_memories += self._knowledge.agent_def.selfl_lore
 
         awaitables = [self._memory.add_memory(memory) for memory in initial_memories]
         await asyncio.gather(*awaitables)
@@ -210,3 +219,160 @@ class GenAgent:
                 queries, top_k=max_memories
             )
         ]
+        
+    def perceive(self, maze: Maze):
+        """
+        Perceives events around the agent and saves it to the memory, both events 
+        and spaces. 
+
+        We first perceive the events nearby the agent, as determined by its 
+        <vision_r>. If there are a lot of events happening within that radius, we 
+        take the <att_bandwidth> of the closest events. Finally, we check whether
+        any of them are new, as determined by <retention>. If they are new, then we
+        save those and return the <ConceptNode> instances for those events. 
+
+        INPUT: 
+            maze: An instance of <Maze> that represents the current maze in which the 
+                agent is acting in. 
+        OUTPUT: 
+            ret_events: a list of <ConceptNode> that are perceived and new. 
+        """
+        # PERCEIVE SPACE
+        # We get the nearby tiles given our current tile and the agent's vision
+        # radius. 
+        nearby_tiles = maze.get_nearby_tiles(self.curr_tile, 
+                                            self.vision_r)
+
+        # We then store the perceived space. Note that the s_mem of the agent is
+        # in the form of a tree constructed using dictionaries. 
+        for i in nearby_tiles: 
+            i = maze.access_tile(i)
+            if i["world"]: 
+                if (i["world"] not in self.s_mem.tree): 
+                    self.s_mem.tree[i["world"]] = {}
+            if i["sector"]: 
+                if (i["sector"] not in self.s_mem.tree[i["world"]]): 
+                    self.s_mem.tree[i["world"]][i["sector"]] = {}
+            if i["arena"]: 
+                if (i["arena"] not in self.s_mem.tree[i["world"]]
+                                                        [i["sector"]]): 
+                    self.s_mem.tree[i["world"]][i["sector"]][i["arena"]] = []
+                if i["game_object"]: 
+                    if (i["game_object"] not in self.s_mem.tree[i["world"]]
+                                                                    [i["sector"]]
+                                                                    [i["arena"]]): 
+                        self.s_mem.tree[i["world"]][i["sector"]][i["arena"]] += [
+                                                                            i["game_object"]]
+
+        # PERCEIVE EVENTS. 
+        # We will perceive events that take place in the same arena as the
+        # agent's current arena. 
+        curr_arena_path = maze.get_tile_path(self.curr_tile, "arena")
+        # We do not perceive the same event twice (this can happen if an object is
+        # extended across multiple tiles).
+        percept_events_set = set()
+        # We will order our percept based on the distance, with the closest ones
+        # getting priorities. 
+        percept_events_list = []
+        # First, we put all events that are occuring in the nearby tiles into the
+        # percept_events_list
+        for tile in nearby_tiles: 
+            tile_details = maze.access_tile(tile)
+            if tile_details["events"]: 
+                if maze.get_tile_path(tile, "arena") == curr_arena_path:  
+                    # This calculates the distance between the agent's current tile, 
+                    # and the target tile.
+                    dist = math.dist([tile[0], tile[1]], 
+                                    [self.curr_tile[0], 
+                                    self.curr_tile[1]])
+                    # Add any relevant events to our temp set/list with the distant info. 
+                    for event in tile_details["events"]: 
+                        if event not in percept_events_set: 
+                            percept_events_list += [[dist, event]]
+                            percept_events_set.add(event)
+
+        # We sort, and perceive only agent.att_bandwidth of the closest
+        # events. If the bandwidth is larger, then it means the agent can perceive
+        # more elements within a small area. 
+        percept_events_list = sorted(percept_events_list, key=itemgetter(0))
+        perceived_events = []
+        for dist, event in percept_events_list[:self.att_bandwidth]: 
+            perceived_events += [event]
+
+        # Storing events. 
+        # <ret_events> is a list of <ConceptNode> instances from the agent's 
+        # associative memory. 
+        ret_events = []
+        for p_event in perceived_events: 
+            s, p, o, desc = p_event
+            if not p: 
+                # If the object is not present, then we default the event to "idle".
+                p = "is"
+                o = "idle"
+                desc = "idle"
+            desc = f"{s.split(':')[-1]} is {desc}"
+            p_event = (s, p, o)
+
+            # We retrieve the latest self.retention events. If there is  
+            # something new that is happening (that is, p_event not in latest_events),
+            # then we add that event to the a_mem and return it. 
+            latest_events = self.a_mem.get_summarized_latest_events(
+                                            self.retention)
+            if p_event not in latest_events:
+                # We start by managing keywords. 
+                keywords = set()
+                sub = p_event[0]
+                obj = p_event[2]
+                if ":" in p_event[0]: 
+                    sub = p_event[0].split(":")[-1]
+                if ":" in p_event[2]: 
+                    obj = p_event[2].split(":")[-1]
+                keywords.update([sub, obj])
+
+                # Get event embedding
+                desc_embedding_in = desc
+                if "(" in desc: 
+                    desc_embedding_in = (desc_embedding_in.split("(")[1]
+                                                        .split(")")[0]
+                                                        .strip())
+                if desc_embedding_in in self.a_mem.embeddings: 
+                    event_embedding = self.a_mem.embeddings[desc_embedding_in]
+                else: 
+                    event_embedding = get_embedding(desc_embedding_in)
+                event_embedding_pair = (desc_embedding_in, event_embedding)
+                
+                # Get event poignancy. 
+                event_poignancy = generate_poig_score(self, 
+                                                        "event", 
+                                                        desc_embedding_in)
+
+                # If we observe the agent's self chat, we include that in the memory
+                # of the agent here. 
+                chat_node_ids = []
+                if p_event[0] == f"{self.name}" and p_event[1] == "chat with": 
+                    curr_event = self.act_event
+                    if self.act_description in self.a_mem.embeddings: 
+                        chat_embedding = self.a_mem.embeddings[
+                                            self.act_description]
+                    else: 
+                        chat_embedding = get_embedding(self
+                                                                .act_description)
+                    chat_embedding_pair = (self.act_description, 
+                                        chat_embedding)
+                    chat_poignancy = generate_poig_score(self, "chat", 
+                                                        self.act_description)
+                    chat_node = self.a_mem.add_chat(self.curr_time, None,
+                                curr_event[0], curr_event[1], curr_event[2], 
+                                self.act_description, keywords, 
+                                chat_poignancy, chat_embedding_pair, 
+                                self.chat)
+                    chat_node_ids = [chat_node.node_id]
+
+                # Finally, we add the current event to the agent's memory. 
+                ret_events += [self.a_mem.add_event(self.curr_time, None,
+                                    s, p, o, desc, keywords, event_poignancy, 
+                                    event_embedding_pair, chat_node_ids)]
+                self.importance_trigger_curr -= event_poignancy
+                self.importance_ele_n += 1
+
+        return ret_events
